4357a611480f83aead04f9bcb421e53574c23a06
Valdir Silva <v.souza.da.silva@avanade.com>
2021-12-22 13:15:20 -0300

    feat: create user header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,34 @@
 import React from 'react'
 
-import { Container, Title } from './styles'
+
+import { 
+    Container, 
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting, 
+    UserName, 
+    Icon
+
+} from './styles'
 
 export function Dashboard(){
     return (
         <Container>
-            <Title>Gofinances</Title>
+           <Header>
+               <UserWrapper>
+                <UserInfo>
+                    <Photo source={{uri: 'https://avatars.githubusercontent.com/u/11434239?v=4'}}  />
+                    <User>
+                        <UserGreeting>Olá,</UserGreeting>
+                        <UserName>Valdir</UserName>
+                    </User>
+                </UserInfo>
+                <Icon name='power'/>
+               </UserWrapper>
+           </Header>
         </Container>
     )
 }
