f2006cb7108f985c2cd816fc894819053432d71e
Valdir Silva <v.souza.da.silva@avanade.com>
2022-01-08 20:31:49 -0300

    feat: configuremenu items

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,3 +1,6 @@
+import { Platform } from 'react-native'
+import { useTheme } from 'styled-components'
+import { MaterialIcons } from '@expo/vector-icons'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs' 
 
 const { Navigator, Screen } = createBottomTabNavigator()
@@ -7,19 +10,59 @@ import { Register } from '../screens/Register'
 
 
 export function AppRoutes(){
+    const theme = useTheme()
+    
     return (
-        <Navigator>
+        <Navigator
+            screenOptions={{
+                headerShown: false,
+                tabBarActiveTintColor: theme.colors.secondary,
+                tabBarInactiveTintColor: theme.colors.text,
+                tabBarLabelPosition: 'beside-icon',
+                tabBarStyle: {
+                    height: 88,
+                    paddingVertical: Platform.OS === 'ios' ? 20 : 0 
+                }
+            }}
+        >
             <Screen 
                 name='dashboard'
                 component={Dashboard}
-            />
+                options={{
+                    tabBarIcon: (({size, color}) => (
+                        <MaterialIcons 
+                            name='format-list-bulleted'
+                            size={size}
+                            color={color}
+                        />
+                    ))
+                }}
+                />
             <Screen 
                 name='Cadastrar'
                 component={Register}
-            />
+                options={{
+                    tabBarIcon: (({size, color}) => (
+                        <MaterialIcons 
+                            name='attach-money'
+                            size={size}
+                            color={color}
+                        />
+                    ))
+                }}
+                />
             <Screen 
                 name='Resumo'
-                component={Dashboard}
+                component={Register}
+                options={{
+                    tabBarIcon: (({size, color}) => (
+                        <MaterialIcons 
+                            name='pie-chart'
+                            size={size}
+                            color={color}
+                        />
+                    ))
+                }}
             />
         </Navigator>
     )
